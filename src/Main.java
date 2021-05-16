fb1e0a4ea95cc8df82fd2194a348a8dd985bf011
Anna Olefirenko <annolefirenko@yandex.ru>
2021-05-16 20:23:09 +0300

    Created two Threads

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,30 @@
-import sorters.BubbleSorter;
-
-import java.util.Arrays;
-import java.util.Scanner;
+import threads.CounterThread;
 
 public class Main {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+        /*Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int [] newArray = new int[n];
         for (int i = 0; i < n; i++){
             newArray[i] = scanner.nextInt();
         }
         System.out.println(Arrays.toString(BubbleSorter.sort(newArray)));
-        System.out.println(Arrays.toString(BubbleSorter.optimizedSort(newArray)));
+        System.out.println(Arrays.toString(BubbleSorter.optimizedSort(newArray)));*/
+        CounterThread counter = new CounterThread();
+        counter.start();
+        Thread charCounter = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 97; i < 103; i++) {
+                    System.out.println((char)i);
+                    try {
+                        Thread.sleep(1500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        charCounter.start();
     }
 }
---
+++ src/threads/CounterThread.java
@@ -0,0 +1,15 @@
+package threads;
+
+public class CounterThread extends Thread{
+    @Override
+    public void run(){
+        for(int i = 0; i < 5; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
